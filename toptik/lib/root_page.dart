e96377a9f485278fc0b382e97aab68c56fe31e19
tmac <727924283@qq.com>
2021-07-08 15:18:56 +0800

    引导页

---
+++ toptik/lib/root_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key key}) : super(key: key);
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("主页"),
+      ),
+    );
+  }
+}
